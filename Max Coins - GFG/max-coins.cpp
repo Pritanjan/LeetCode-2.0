055e950ee45f4745775fa6e75ca157bc1b615daa
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2023-05-04 11:43:04 +0530

    Added solution - LeetHub

---
+++ Max Coins - GFG/max-coins.cpp
@@ -0,0 +1,70 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution {
+public:
+    int maxCoins(int n, vector<vector<int>>& ranges) {
+        sort(ranges.begin(), ranges.end());
+        vector<vector<int>> dp(ranges.size(), vector<int>(3, -1));
+        return solve(ranges, 0, 2, dp);
+    }
+    
+    int solve(vector<vector<int>>& v, int i, int rem, vector<vector<int>>& dp) {
+        // rem = number of intervals need to be chosen (remaining)
+        if (rem == 0 || i == v.size()) return 0;
+        
+        if (dp[i][rem] != -1) return dp[i][rem];
+        
+        int nt = solve(v, i+1, rem, dp); // notTake
+        int t = v[i][2]; // t
+        
+        int idx = binarySearch(v, i);
+        t += solve(v, idx, rem - 1, dp); //note that we passed idx and not i+1
+        return dp[i][rem] = max(t, nt);
+    }
+    
+    int binarySearch(vector<vector<int>>& v, int i) {
+        int low = i + 1, high = v.size() - 1;
+        int idx = v.size(); // in case every interval is overlapping
+        
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (v[mid][0] >= v[i][1]) {
+                idx = mid;
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        
+        return idx;
+    }
+};
+
+
+
+//{ Driver Code Starts.
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        vector<vector<int>> a(n,vector<int>(3));
+        for(int i=0;i<n;i++){
+            cin>>a[i][0]>>a[i][1]>>a[i][2];
+        }
+        Solution ob;
+        cout<<ob.maxCoins(n,a)<<endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
